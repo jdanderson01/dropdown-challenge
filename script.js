007d03ff0f1ceb9db9e9e38a554945f6368e7b68
jdanderson01 <74023705+jdanderson01@users.noreply.github.com>
2024-10-18 21:24:49 -0400

    Base mobile design
    Initial mobile design for the FEM dropdown challenge

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger-menu").addEventListener("click", function () {
+    const navMenu = document.querySelector(".nav-menu");
+    navMenu.classList.toggle("show");
+  });
